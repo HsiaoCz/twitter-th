31ef0d9b806d8ef11d4e3ce8a06d58130f76ee1e
HsiaoCz <984274788@qq.com>
2024-06-07 10:49:35 +0800

    first layout

---
+++ cmd/app/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"twitter-th/app"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/anthdm/gothkit/kit"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewMux()
+
+	app.InitializeMiddleware(router)
+
+	if kit.IsDevelopment() {
+		router.Handle("/public/*", disableCache(staticDev()))
+	}
+
+	kit.UseErrorHandler(app.ErrorHandler)
+	router.HandleFunc("/*", kit.Handler(app.NotFoundHandler))
+
+	app.InitializeRoutes(router)
+
+	fmt.Printf("application running in %s at %s\n", kit.Env(), "http://localhost:7331")
+
+	http.ListenAndServe(os.Getenv("HTTP_LISTEN_ADDR"), router)
+}
+
+func staticDev() http.Handler {
+	return http.StripPrefix("/public/", http.FileServerFS(os.DirFS("public")))
+}
+
+func disableCache(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
